892e2662a2162a162d5bbacf6c976fb8e765f2e2
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-14 19:15:03 +0800

    Marked the PetOwner as entity and set the pets list as transient

--- src/main/java/com/petpulse/model/PetOwner.java
+++ src/main/java/com/petpulse/model/PetOwner.java
@@ -1,10 +1,20 @@
 package com.petpulse.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class PetOwner extends User {
+
+    // @Transient tells JPA to ignore this field in the database for now
+    // until Pet is also converted into an @Entity
+    @Transient
     private List<Pet> pets;
+
+    // Default constructor required by Spring Data JPA
+    public PetOwner() {}
 
     public PetOwner(String id, String name, String password) {
         super(id, name, password);
