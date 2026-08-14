7a3aa67c99f17a82886cdfd446c22b265faaee98
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-14 19:08:05 +0800

    Created Vet domain model with medical license

--- src/main/java/com/petpulse/model/Vet.java
+++ src/main/java/com/petpulse/model/Vet.java
@@ -1,4 +1,17 @@
 package com.petpulse.model;
 
-public class Vet {
+public class Vet extends User {
+    private String medicalLicense;
+
+    public Vet(String id, String name, String password, String medicalLicense) {
+        super(id, name, password);
+        this.medicalLicense = medicalLicense;
+    }
+
+    public String getMedicalLicense() {
+        return medicalLicense;
+    }
+    public void setMedicalLicense(String medicalLicense) {
+        this.medicalLicense = medicalLicense;
+    }
 }
