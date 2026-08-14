6ec0765d180ec4c0aca14a44bc52bca50aa4867a
Denise Verzosa <denise_verzosa@dlsu.edu.ph>
2026-08-14 20:16:04 +0800

    Created the AppointmentDTO structure and define class fields (AppointmentDTO)

---
+++ src/main/java/com/petpulse/dto/AppointmentDTO.java
@@ -0,0 +1,10 @@
+package com.petpulse.dto;
+
+import java.time.LocalDateTime;
+
+public class AppointmentDTO {
+    private String id;
+    private String petName;
+    private String vetName;
+    private LocalDateTime timeSlot;
+}
