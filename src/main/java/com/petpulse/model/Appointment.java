de02eebdf15ba3eaa34ef2d5ae9e3c5e1c6b7494
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-14 19:33:32 +0800

    Added Vet, PetOwner, and Pet with Appointment

--- src/main/java/com/petpulse/model/Appointment.java
+++ src/main/java/com/petpulse/model/Appointment.java
@@ -4,17 +4,32 @@ import java.time.LocalDateTime;
 
 public class Appointment {
     private String id;
+    private Vet vet;
+    private PetOwner owner;
+    private Pet pet;
     private LocalDateTime timeSlot;
     private String status;
 
     public Appointment(String id, LocalDateTime timeSlot) {
         this.id = id;
+        this.vet = vet;
+        this.owner = owner;
+        this.pet = pet;
         this.timeSlot = timeSlot;
         this.status = "Confirmed";
     }
 
     public String getId() {
         return id;
+    }
+    public Vet getVet() {
+        return vet;
+    }
+    public PetOwner getOwner() {
+        return owner;
+    }
+    public Pet getPet() {
+        return pet;
     }
     public LocalDateTime getTimeSlot() {
         return timeSlot;
