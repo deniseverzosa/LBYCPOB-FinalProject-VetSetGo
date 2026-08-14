6979dd3dd78141e16f3a82a2a26533fe5831c1ca
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-14 19:13:13 +0800

    Created the PetOwner domain model

--- src/main/java/com/petpulse/model/PetOwner.java
+++ src/main/java/com/petpulse/model/PetOwner.java
@@ -1,4 +1,20 @@
 package com.petpulse.model;
 
-public class PetOwner {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetOwner extends User {
+    private List<Pet> pets;
+
+    public PetOwner(String id, String name, String password) {
+        super(id, name, password);
+        this.pets = new ArrayList<>();
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+    public void addPet(Pet pet) {
+        this.pets.add(pet);
+    }
 }
