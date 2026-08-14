4b0775c5bac980851b8d75509822ecee34e5a1dc
Zach <zachary_acosta@dlsu.edu.ph>
2026-08-14 22:03:25 +0800

    added needed resources for the JsonDataHandler class

---
+++ src/main/java/com/petpulse/service/JsonDataHandler.java
@@ -0,0 +1,12 @@
+package com.petpulse.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class JsonDataHandler {
+}
