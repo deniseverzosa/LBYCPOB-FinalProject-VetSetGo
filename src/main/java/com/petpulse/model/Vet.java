fcf958fb4a01e7153e44a6db585095552ffc21b4
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-14 19:09:24 +0800

    Marked the Vet class as a JPA entity

--- src/main/java/com/petpulse/model/Vet.java
+++ src/main/java/com/petpulse/model/Vet.java
@@ -1,7 +1,13 @@
 package com.petpulse.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Vet extends User {
     private String medicalLicense;
+
+    // Default constructor required by Spring Data JPA
+    public Vet() {}
 
     public Vet(String id, String name, String password, String medicalLicense) {
         super(id, name, password);
