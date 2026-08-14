32f84e33e19f094a582f3d54065c9385cffff60c
Denise Verzosa <denise_verzosa@dlsu.edu.ph>
2026-08-14 16:03:51 +0800

    Added the MedicalRecord class and defined properties for date, diagnosis, and medications

---
+++ src/main/java/com/petpulse/model/MedicalRecord.java
@@ -0,0 +1,10 @@
+package com.petpulse.model;
+
+import java.time.LocalDate;
+
+public class MedicalRecord {
+    private LocalDate date;
+    private String diagnosis;
+    private String medications;
+
+}
