113e026f0d97cba1ea1e861616258eef444e4b7b
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-16 00:52:33 +0800

    Let Vaccination class extends to MedicalService

--- src/main/java/com/vetsetgo/model/Vaccination.java
+++ src/main/java/com/vetsetgo/model/Vaccination.java
@@ -1,4 +1,20 @@
 package com.vetsetgo.model;
 
-public class Vaccination {
+public class Vaccination extends MedicalService {
+    private String vaccineName;
+
+    public Vaccination(String vaccineName, double baseFee) {
+        super("Vaccination", baseFee);
+        this.vaccineName = vaccineName;
+    }
+
+    @Override
+    public double calculateCost() {
+        return getBaseFee(); // Fixed price for vaccine
+    }
+
+    @Override
+    public String getDetails() {
+        return "Administered " + vaccineName + " vaccine";
+    }
 }
