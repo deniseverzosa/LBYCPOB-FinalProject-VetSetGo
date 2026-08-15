6f136928d5494f4f2274b01f1ec9c3688344733f
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-16 01:18:55 +0800

    Initialized default Vet user if database is empty

--- src/main/java/com/vetsetgo/config/DatabaseSeeder.java
+++ src/main/java/com/vetsetgo/config/DatabaseSeeder.java
@@ -1,5 +1,6 @@
 package com.vetsetgo.config;
 
+import com.vetsetgo.model.Vet;
 import com.vetsetgo.repository.VetRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,12 @@ public class DatabaseSeeder {
     @Bean
     public CommandLineRunner seedDatabase(VetRepository vetRepository) {
         return args -> {
-            // TODO: Implement seeding logic to create default entities
+            // If the database is empty, create a default Vet
+            if (vetRepository.count() == 0) {
+                Vet vet = new Vet("V202", "Dr. Bob Miller", "vetpass", "drbob@email.com", "555-9876", "VET-LICENSE-99");
+                vetRepository.save(vet);
+                System.out.println("Default Vet Initialized - ID: V202 | Password: vetpass");
+            }
         };
     }
 }
