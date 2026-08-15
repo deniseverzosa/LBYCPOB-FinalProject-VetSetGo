779068a549f765b7feb771590a868a74ea4b69ba
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-16 01:17:47 +0800

    Added CommandLineRunner bean skeleton

--- src/main/java/com/vetsetgo/config/DatabaseSeeder.java
+++ src/main/java/com/vetsetgo/config/DatabaseSeeder.java
@@ -1,4 +1,17 @@
 package com.vetsetgo.config;
 
+import com.vetsetgo.repository.VetRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseSeeder {
+
+    @Bean
+    public CommandLineRunner seedDatabase(VetRepository vetRepository) {
+        return args -> {
+            // TODO: Implement seeding logic to create default entities
+        };
+    }
 }
