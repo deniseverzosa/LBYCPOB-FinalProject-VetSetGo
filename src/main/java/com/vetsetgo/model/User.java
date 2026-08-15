ba0d125d3e7564d2cfa95c967127881f6e159203
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-15 13:34:03 +0800

    Added getters and setters for the contact fields in User class

--- src/main/java/com/vetsetgo/model/User.java
+++ src/main/java/com/vetsetgo/model/User.java
@@ -45,4 +45,9 @@ public abstract class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 }
