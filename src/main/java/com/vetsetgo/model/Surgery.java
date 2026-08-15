e7a6706b2e30745fff5362ee5375222025fb5d00
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-16 00:54:02 +0800

    Surgery class extends MedicalServices

--- src/main/java/com/vetsetgo/model/Surgery.java
+++ src/main/java/com/vetsetgo/model/Surgery.java
@@ -1,4 +1,20 @@
 package com.vetsetgo.model;
 
-public class Surgery {
+public class Surgery extends MedicalService {
+    private double anesthesiaFee;
+
+    public Surgery(String surgeryType, double baseFee, double anesthesiaFee) {
+        super("Surgery (" + surgeryType + ")", baseFee);
+        this.anesthesiaFee = anesthesiaFee;
+    }
+
+    @Override
+    public double calculateCost() {
+        return getBaseFee() + anesthesiaFee; // Base surgical fee plus additional fees
+    }
+
+    @Override
+    public String getDetails() {
+        return getServiceName() + " [Anesthesia: $" + anesthesiaFee + "]";
+    }
 }
