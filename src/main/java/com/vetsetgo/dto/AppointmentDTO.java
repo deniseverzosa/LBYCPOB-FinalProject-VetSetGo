bb4403347f8be4e0b941c81a2d011eea791ca30c
XuCatherine1023 <catherine_xu@dlsu.edu.ph>
2026-08-15 22:29:35 +0800

    Added vetId to AppointmentDTO

--- src/main/java/com/vetsetgo/dto/AppointmentDTO.java
+++ src/main/java/com/vetsetgo/dto/AppointmentDTO.java
@@ -6,6 +6,7 @@ public class AppointmentDTO {
     private String id;
     private String petName;
     private String vetName;
+    private String vetId;
     private LocalDateTime timeSlot;
 
     public AppointmentDTO() {}
@@ -14,6 +15,7 @@ public class AppointmentDTO {
         this.id = id;
         this.petName = petName;
         this.vetName = vetName;
+        this.vetId = vetId;
         this.timeSlot = timeSlot;
     }
 
@@ -25,6 +27,9 @@ public class AppointmentDTO {
 
     public String getVetName() { return vetName; }
     public void setVetName(String vetName) { this.vetName = vetName; }
+
+    public String getVetId() { return vetId; }
+    public void setVetId(String vetId) { this.vetId = vetId; }
 
     public LocalDateTime getTimeSlot() { return timeSlot; }
     public void setTimeSlot(LocalDateTime timeSlot) { this.timeSlot = timeSlot; }
