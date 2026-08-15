a99b41ffd9e27085581e8269969f77c68f7807ce
Zach <zachary_acosta@dlsu.edu.ph>
2026-08-15 22:25:33 +0800

    created VetRepository interface

--- src/main/java/com/vetsetgo/repository/VetRepository.java
+++ src/main/java/com/vetsetgo/repository/VetRepository.java
@@ -1,4 +1,10 @@
 package com.vetsetgo.repository;
 
-public class VetRepository {
+import com.vetsetgo.model.Vet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VetRepository extends JpaRepository<Vet, String> {
+
 }
