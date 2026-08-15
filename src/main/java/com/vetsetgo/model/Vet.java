187b2e75c4aece29ad88bdcdf35b182deb821e93
Denise Verzosa <denise_verzosa@dlsu.edu.ph>
2026-08-15 21:09:43 +0800

    Added parameterized constructor to initialize Vet fields and collections

--- src/main/java/com/vetsetgo/model/Vet.java
+++ src/main/java/com/vetsetgo/model/Vet.java
@@ -13,4 +13,13 @@ public class Vet extends User {
 
     @OneToMany(mappedBy = "vet", cascade = CascadeType.ALL)
     private List<Appointment> upcomingAppointments;
+
+    public Vet() {}
+
+    public Vet(String id, String name, String password, String email, String phoneNumber, String medicalLicense) {
+        super(id, name, password, email, phoneNumber);
+        this.medicalLicense = medicalLicense;
+        this.availableShifts = new ArrayList<>();
+        this.upcomingAppointments = new ArrayList<>();
+    }
 }
