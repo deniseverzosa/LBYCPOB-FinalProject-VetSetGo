f4199b6ab252e1f8a010087bf9d4ab248180f57e
Denise Verzosa <denise_verzosa@dlsu.edu.ph>
2026-08-18 21:54:00 +0800

    DELETED the service package: upgraded the system to a better database (Spring Data JPA)

--- src/main/java/com/vetsetgo/service/DataHandler.java
+++
@@ -1,9 +0,0 @@
-package com.vetsetgo.service;
-
-import java.util.List;
-
-// Abstraction: Interface for persistent data storage
-public interface DataHandler<T> {
-    void save(List<T> data, String filename);
-    List<T> load(String filename, Class<T[]> clazz);
-}
